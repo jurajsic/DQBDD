fb256d579b9c4bbc1e14603ba0199e256a53c8a5
Juraj Sic <juraj.sic@outlook.com>
2020-03-26 16:55:43 +0100

    added simple HQSpre

---
+++ src/hqspresimple.cpp
@@ -0,0 +1,45 @@
+#include <easylogging++.hpp>
+#include <formula.hpp>
+
+INITIALIZE_EASYLOGGINGPP
+
+int main(int argc, char **argv)
+{
+    hqspre::Formula formula;
+
+    // Configure logging
+    el::Configurations defaultConf;
+    defaultConf.setToDefault();
+    defaultConf.setGlobally(el::ConfigurationType::Enabled, "true");
+    defaultConf.setGlobally(el::ConfigurationType::Format, "[HQSpre] %msg");
+    defaultConf.set(el::Level::Verbose, el::ConfigurationType::Format, "[HQSpre] %msg");
+    defaultConf.setGlobally(el::ConfigurationType::ToFile, "false");
+    defaultConf.setGlobally(el::ConfigurationType::ToStandardOutput, "true");
+    el::Loggers::reconfigureAllLoggers(defaultConf);
+    el::Loggers::setVerboseLevel(0);
+
+    formula.settings().consistency_check = false;
+
+    // Parse the file
+    std::string in_name(argv[1]);
+    std::ifstream in(in_name);
+    in >> formula;
+    in.close();
+
+    // do the preprocessing magic
+    try {
+        formula.settings().bla              = false;
+        formula.settings().ble              = false;
+        formula.settings().pure_sat_timeout = 1000;
+        formula.preprocess();
+        //formula.printStatistics();
+    } catch (hqspre::SATException&) {
+        std::cout << "p cnf 0 0\n" << std::endl;
+        return 10;
+    } catch (hqspre::UNSATException&) {
+        std::cout << "p cnf 0 1\n0\n" << std::endl;
+        return 20;
+    }
+
+    std::cout << formula;
+}
