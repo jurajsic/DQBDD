9cd87f3fb4bf9f47ad30352531a0cefcd327e365
jurajsic <juraj.sic@outlook.com>
2021-03-29 18:26:54 +0200

    bumped version to v1.2-beta5

--- src/main.cpp
+++ src/main.cpp
@@ -87,7 +87,7 @@ int main(int argc, char **argv)
     }
 
     if (result->count("version")) {
-        std::cout << "DQBDD 1.2-beta4" << std::endl;
+        std::cout << "DQBDD 1.2-beta5" << std::endl;
         return 0;
     }
     
